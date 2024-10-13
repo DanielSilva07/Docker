2ac296cfe9f5d0f10e6e4d180754cb254efecd97
danielsilva07 <daniel97silva.ds@gmail.com>
2024-10-13 17:09:55 -0300

    fist-commit

---
+++ src/main/java/com/example/Docker/controller/DockerController.java
@@ -0,0 +1,13 @@
+package com.example.Docker.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DockerController {
+
+    @GetMapping("/gg")
+    public String teste(){
+        return "!!! MIAMI HEAT !!!";
+    }
+
+}
