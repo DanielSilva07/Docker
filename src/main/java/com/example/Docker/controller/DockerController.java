187e3e9966820e3201c2be16027de0ce3d574a0d
danielsilva07 <daniel97silva.ds@gmail.com>
2024-10-18 18:03:32 -0300

    ci-cd

--- src/main/java/com/example/Docker/controller/DockerController.java
+++ src/main/java/com/example/Docker/controller/DockerController.java
@@ -1,4 +1,5 @@
 package com.example.Docker.controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -6,8 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class DockerController {
 
     @GetMapping("/gg")
-    public String teste(){
-        return "!!! MIAMI HEAT $$ !!!!!!";
+    public ResponseEntity<String> hello(){
+        return ResponseEntity.ok().body("MIAMI HEAT $$ !!!!!!");
     }
 
 }
